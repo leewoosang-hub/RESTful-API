34d48b04169bbe401e9c67471f32fcd5f6d7b17d
lee woosang <dntkds@naver.com>
2025-03-10 18:27:33 +0900

    依存性注入

---
+++ board/src/main/java/com/restful/app/web/BoardControl.java
@@ -0,0 +1,13 @@
+package com.restful.app.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restful.app.service.BoardService;
+
+@RestController
+public class BoardControl {
+	
+	private BoardService boardService;
+		
+
+}
